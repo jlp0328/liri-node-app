08e91f54d31ccc053ad2d5ace9569f94a2484320
jlp0328 <jlp0328@gmail.com>
2017-02-08 22:29:53 -0500

    all functions are working

--- liri.js
+++ liri.js
@@ -1,16 +1,14 @@
-var twitter = require("./keys.js");
+var keys = require("./keys.js");
 var spotify = require("spotify");
+var twitter = require("twitter");
+// var tmdb = require("tmdb").init({apikey:"798d16e44bf2357493e013567298dcd5"});
+// var TmdbApi = require("tmdb-api");
 var request = require("request");
+var fs = require("fs");
 
-// var twitterKeys = keys.twitterKeys;
 
-// var client = new twitter({
-//   consumer_key: twitterKeys.consumer_key;
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// console.log(twitterKeys);
 
-// });
 var nodeArgs = process.argv;
 var query = [];
 // var action = process.argv.slice(2);
@@ -28,7 +26,6 @@ var value = String(argTwo);
 
 console.log("Searching for " +  value);
 console.log("What command? " + action);
-
 
 
 switch (action){
@@ -50,51 +47,166 @@ switch (action){
 
 }
 
-
-
-
-
 //Functions
 
 //Commands for Liri to take in...
 // * `my-tweets`
-function myTweets(){}
+function myTweets(){
 
+var twitterKeys = keys.twitterKeys;
 
-// * `spotify-this-song`
-function spotifyThisSong (){
+var client = new twitter({
+  consumer_key: twitterKeys.consumer_key,
+  consumer_secret: twitterKeys.consumer_secret,
+  access_token_key: twitterKeys.access_token_key,
+  access_token_secret: twitterKeys.access_token_secret
+});
 
-  spotify.search({ type:"track", query: value},
-    function(err, data){
-      if (err) {
-        console.log("Error occurred: " + err);
-        return;
-      }
+var params = {screen_name: "jlp0328", count:20};
 
-  // console.log(JSON.stringify(data, null, 2));
+client.get("statuses/user_timeline", params, function(error, tweets, response) {
+  if (error) {
+    console.log(error);
+  }
 
-  var results = data.tracks.items[0];
-
-  var artist = results.artists.name;
-  // var songName = results[0].
-  var songLink = results.external_urls.spotify;
-  var album = results.album.name;
-
-  //Need: artist(s), song's name, preview link of song, album//
-  console.log("************");
-  console.log("Artist: " + artist);
-  console.log("Song Link: " + songLink);
-  console.log("Album: " + album);
-
+  for(var i = 0; i < tweets.length; i++){
+    console.log("************");
+    console.log(tweets[i].text);
+    console.log("************");
+  }
 
 });
 }
 
+// * `spotify-this-song`
+function spotifyThisSong (){
 
+  spotify.search({
+    type:"track",
+    query: value}, function(err, data){
+
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+  // * if no song is provided then your program will default to
+  //   * "The Sign" by Ace of Base
+  if(value === ""){
+      console.log("************");
+      console.log("Artist: Ace of Base");
+      console.log("Song: The Sign");
+      console.log("Song Link: https://open.spotify.com/track/0hrBpAOgrt8RXigk83LLNE");
+      console.log("Album: The Sign");
+      console.log("************");
+  }
+  else{
+
+  for (i = 0; i < 5; i++){
+
+      var results = data.tracks.items[i];
+
+      var artist = results.artists[0].name;
+      var songName = results.name;
+      var songLink = results.external_urls.spotify;
+      var album = results.album.name;
+
+      //Need: artist(s), song's name, preview link of song, album//
+      console.log("************");
+      console.log("Artist: " + artist);
+      console.log("Song: " + songName);
+      console.log("Song Link: " + songLink);
+      console.log("Album: " + album);
+      console.log("************");
+    }
+}
+
+});
+
+}
 
 // * `movie-this`
-function movieThis(){}
+function movieThis(){
+
+  var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=798d16e44bf2357493e013567298dcd5&query=" + value;
+
+request(queryURL, function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (error) {
+    console.log("Error occurred: " + error);
+    return;
+  }
+
+  if(value === ""){
+
+    console.log("************");
+    console.log("Movie Name: Mr.Nobody");
+    console.log("Release Date: 2009-09-11");
+    console.log("Synopsis: Nemo Nobody leads an ordinary existence with his wife and 3 children; one day, he wakes up as a mortal centenarian in the year 2092.");
+    console.log("Average Vote: 7.9");
+    console.log("Language: en");
+    console.log("************");
+
+  }
+
+  else{
+    console.log("************");
+    console.log("Movie Name: " + JSON.parse(body).results[0].title);
+    console.log("Release Date: " + JSON.parse(body).results[0].release_date);
+    console.log("Synopsis: " + JSON.parse(body).results[0].overview);
+    console.log("Average Vote: " + JSON.parse(body).results[0].vote_average);
+    console.log("Language: " + JSON.parse(body).results[0].original_language);
+    console.log("************");
+}
+});
+
+}
+
+// * Title of the movie.
+//     * Year the movie came out.
+//     * IMDB Rating of the movie.
+//     * Country where the movie was produced.
+//     * Language of the movie.
+//     * Plot of the movie.
+//     * Actors in the movie.
+//     * Rotten Tomatoes Rating.
+//     * Rotten Tomatoes URL.
+
 
 
 // * `do-what-it-says`
-function doThis(){}
+function doThis(){
+
+// Feel free to change the text in that document to test out the feature for other commands.
+fs.readFile("random.txt", "utf8", function(error,data){
+
+  var content = data.split(",");
+
+  // var array = data.toString().split("\n");
+  // console.log(array);
+
+  action = content[0];
+  value = content[1];
+
+  switch (action){
+  case "my-tweets":
+  myTweets();
+  break;
+
+  case "spotify-this-song":
+  spotifyThisSong();
+  break;
+
+  case "movie-this":
+  movieThis();
+  break;
+
+  case "do-what-it-says":
+  doThis();
+  break;
+
+}
+
+});
+
+}
